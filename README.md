cebca6973824409fed05ffbd9ff16dc27ad38aad
Elrondo46 <elrond94@hotmail.com>
2020-12-26 17:05:45 +0100

    Fist Release

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# davical-docker-standalone
-Standalone davical docker container
+# DAViCal Standalone Docker
+Docker image for a complete [DAViCal](https://www.davical.org/) server (DAViCal + Apache2 + PostgreSQL) on Alpine Linux requires a separate PGSQL container (watch docker-compose).
+The repository on github.org contains example configuration files for DAViCal (as well as the Dockerfile to create the Docker image).
+It contains docker-compose config example
+
+### About DAViCal
+[DAViCal](https://www.davical.org/) is a server for shared calendars. It implements the [CalDAV protocol](https://wikipedia.org/wiki/CalDAV) and stores calendars in the [iCalendar format](https://wikipedia.org/wiki/ICalendar).
+
+List of supported clients: Mozilla Thunderbird/Lightning, Evolution, Mulberry, Chandler, iCal, ...
+
+**Features**
+>    - DAViCal is Free Software licensed under the General Public License.
+>    - uses an PGSQL database for storage of event data
+>    - supports backward-compatible access via WebDAV in read-only or read-write mode (not recommended)
+>    - is committed to inter-operation with the widest possible CalDAV client software.
+>
+>DAViCal supports basic delegation of read/write access among calendar users, multiple users or clients reading and writing the same calendar entries over time, and scheduling of meetings with free/busy time displayed.
+(*https://www.davical.org/*)
+
+### Settings Added
+- Exposed Ports: TCP 80 and TCP 443
+- Exposed Volumes: /config and /var/lib/postgresql/data/
+
+### Multilanguage Support for Interface
+
+> - DAVICAL_LANG is here for this
+> - Diffrents values for languages: en, ar, de_DE, es_AR, es_ES, es_MX, es_VE, et_EE, fe_FI, fr_FR, hu_HU, it_IT, ja_JP, ko_KR, nb_NO, nl_NL, pl_PL, pt_BR, pt_PT, ru_RU, sk_SK, sv_SE
+
+### Multi Architecture Support
+You can use it in ARM, ARM64 and AMD64
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#SET THE TIMEZONE
+apk add --update tzdata
+cp /usr/share/zoneinfo/$TIME_ZONE /etc/localtime
+echo $TIME_ZONE > /etc/timezone
+apk del tzdata
+
+#PREPARE THE PERMISSIONS FOR VOLUMES
+chown -R root:root /config
+chmod -R 755 /config
+mv -n 	/apache.conf /config/apache.conf
+mv -n	/davical.php /config/davical.php
+mv -n	/rsyslog.conf /config/rsyslog.conf
+sed -i 's@host=example@'"host=$DBHOST"'@' /config/davical.php
+sed -i 's@password=example@'"password=$PASSDAVDB"'@' /config/davical.php
+chown -R root:root /config
+chmod -R 755 /config
+chown root:apache /config/davical.php
+chmod u+rwx,g+rx /config/davical.php
+
+sleep 3
+INITIALIZED_DB=$(PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -l | grep davical) 
+if [[ -z "$INITIALIZED_DB" ]] ; then
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c 'CREATE DATABASE davical;'
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c 'CREATE ROLE davical_dba;'
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c 'CREATE ROLE davical_app;'
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c "ALTER USER davical_dba WITH PASSWORD '$PASSDAVDB';"
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c "ALTER USER davical_app WITH PASSWORD '$PASSDAVDB';"
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c 'GRANT ALL PRIVILEGES ON DATABASE davical TO davical_dba;'
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c 'GRANT ALL PRIVILEGES ON DATABASE davical TO davical_app;'
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c 'ALTER USER davical_dba WITH LOGIN;'
+PGPASSWORD=$PGSQL_ROOT_PASS /usr/bin/psql -qX -U postgres -h $DBHOST -c 'ALTER USER davical_app WITH LOGIN;'
+PGPASSWORD=$PASSDAVDB /usr/bin/psql -qXAt -U davical_dba -h $DBHOST davical < /usr/share/awl/dba/awl-tables.sql
+PGPASSWORD=$PASSDAVDB /usr/bin/psql -qXAt -U davical_dba -h $DBHOST davical < /usr/share/awl/dba/schema-management.sql
+PGPASSWORD=$PASSDAVDB /usr/bin/psql -qXAt -U davical_dba -h $DBHOST davical < /usr/share/davical/dba/davical.sql
+PGPASSWORD=$PASSDAVDB /usr/share/davical/dba/update-davical-database --dbname davical --dbuser davical_dba --dbhost $DBHOST --dbpass $PASSDAVDB --appuser davical_app --nopatch --owner davical_dba
+PGPASSWORD=$PASSDAVDB /usr/bin/psql -qXAt -U davical_dba -h $DBHOST davical < /usr/share/davical/dba/base-data.sql
+PGPASSWORD=$PASSDAVDB /usr/bin/psql -qX -U davical_dba -h $DBHOST -c "UPDATE usr SET password = '**$ADMINDAVICALPASS' WHERE user_no = 1;" davical
+fi
+
+#UPDATE ALWAYS THE DATABASE
+sleep 3
+
+/usr/share/davical/dba/update-davical-database --dbname davical --dbuser davical_dba --dbhost $DBHOST --dbpass $PASSDAVDB --appuser davical_app --nopatch --owner davical_dba
+
+#LAUNCH THE INIT PROCESS
+exec /usr/sbin/httpd -e error -E /var/log/apache2/apache-start.log -DFOREGROUND
+
