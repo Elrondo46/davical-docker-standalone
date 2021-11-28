94aa7a63db742a880e59513a671ea0e427f44bd5
Elrondo46 <elrond94@hotmail.com>
2021-11-28 23:18:09 +0100

    Reverting

--- davical.php
+++ davical.php
@@ -4,7 +4,7 @@
 *            These apply everywhere and will need setting                  *
 *                                                                          *
 ***************************************************************************/
-$c->domain_name = $_ENV['HOST_NAME'];
+$c->domain_name = getenv('HOST_NAME')?:'davical.example';
 $c->sysabbr = 'davical';
 
 /****************************
@@ -387,8 +387,7 @@ $c->default_privileges = array('all');
 * People interested in providing new translations are directed to the Wiki:
 *   http://wiki.davical.org/w/Translating_DAViCal
 */
-// $c->default_locale = getenv("DAVICAL_LANG")?:'en_US';
-$c->default_locale = $_ENV['DB_NAME'];
+$c->default_locale = getenv("DAVICAL_LANG")?:'en_US';
 
 /**
 * Default will be $_SERVER['SERVER_NAME'];
